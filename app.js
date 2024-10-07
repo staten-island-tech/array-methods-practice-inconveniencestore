f543af0a35a5ce4a5d720646182b4ae4ac4537d0
inconveniencestore <iivyleaves@gmail.com>
2024-10-07 12:19:53 -0400

    :(

---
+++ app.js
@@ -0,0 +1,9 @@
+const students = [
+    { name: 'Alice', age: 20, grade: 85 },
+    { name: 'Bob', age: 22, grade: 90 },
+    { name: 'Charlie', age: 21, grade: 78 },
+    { name: 'Diana', age: 23, grade: 95 },
+  ];
+
+  students.forEach((students) => console.log(students.name));
+  students.forEach((students) => console.log(students.grade));
